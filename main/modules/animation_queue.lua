cf59b3c9e1bd4cd404cf89ddf3837b680aa178dc
3D23 <dmitrichenkov.dima@mail.ru>
2024-12-02 14:40:46 +0300

    add animation queue

---
+++ main/modules/animation_queue.lua
@@ -0,0 +1,54 @@
+Object = require("main.class")
+
+Animation_queue = Object:extend()
+
+Animation_queue.start_animation = function ()
+    local min_priority = Animation_queue.animations[1].priority
+    pprint(min_priority)
+    for _, value in ipairs(Animation_queue.animations) do
+        if value.priority == min_priority and 
+            coroutine.status(value.animation) == "suspended" then
+                coroutine.resume(value.animation)
+        end
+    end
+end
+
+Animation_queue.on_animation_complete = function ()
+    table.remove(Animation_queue.animations, 1)
+    if #Animation_queue.animations == 0 then return end
+    Animation_queue.start_animation()
+end
+
+Animation_queue.comparable = function (el1, el2)
+    return el1.priority < el2.priority
+end
+
+function Animation_queue:new()
+    Animation_queue.animations = {}
+end
+
+---@param animation thread
+---@param priority integer
+function Animation_queue:add_animation(animation, priority)
+    table.insert(Animation_queue.animations, 
+        {
+            animation = animation,
+            priority = priority
+        })
+    table.sort(Animation_queue.animations, Animation_queue.comparable)
+    Animation_queue.start_animation()
+end
+
+
+---@return
+---| '"running"'
+---| '"dead"'      
+function Animation_queue:status()
+    if #Animation_queue.animations > 0 then
+        return "running"
+    else
+        return "dead"
+    end
+end
+
+return Animation_queue
---
+++ main/modules/animation_queue_module.lua
@@ -0,0 +1,6 @@
+Animation_queue = require("main.modules.animation_queue")
+
+local animation_queue_module = {}
+animation_queue_module.animation_queue = Animation_queue()
+
+return animation_queue_module
